29f0f339f425f3e70cd121573de1e306eae0fa27
twant <taylor.want@gmail.com>
2017-08-10 08:56:22 -0400

    Update transfer_spec.rb

--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -33,7 +33,7 @@ describe 'Transfer' do
 
   it "calls on the sender and reciever's #valid? methods" do
     transfer_class = File.read("lib/transfer.rb")
-    expect(transfer_class.scan(/sender.valid\? \&\& receiver.valid\?/).length).to eq 1
+    expect(transfer_class.scan(/@sender.valid\? \&\& @receiver.valid\?/).length).to eq 1
   end
 
   context "Making transfers between two accounts" do
